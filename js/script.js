a72a9d562a7bf8c7fd004d4fc95894fded2b5645
zaxweb <putrasikumbang@gmail.com>
2023-01-27 21:35:06 +0700

    #

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 7, 2023 01:00:00").getTime();
+var countDownDate = new Date("Jan 30, 2023 01:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
@@ -16,7 +16,7 @@ var x = setInterval(function () {
     if (distance < 0) {
         clearInterval(x);
 
-        var countDownDate1 = new Date("Feb 7, 2023 01:00:00").getTime();
+        var countDownDate1 = new Date("Feb 25, 2023 01:00:00").getTime();
         var y = setInterval(function () {
             var now1 = new Date().getTime();
             var distance1 = countDownDate1 - now1;
